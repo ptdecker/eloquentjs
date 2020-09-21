5520591b718b3a6f943adccd0700974185e9f9bf
P. Todd Decker <ptdecker@mac.com>
2020-09-20 21:12:00 -0500

    Chapter 15 Partial (Mouse Trail) - Uses DOM directly instead of a separate trail history queue - Uses a null clock value to determine need for trail reaping - Moved reaper into listener with arrow function

--- chapter15/mousetrail/scripts.js
+++ chapter15/mousetrail/scripts.js
@@ -3,23 +3,31 @@
 // JavaScript loaed by index.html in support of exercise
 
 const maxTrailLength = 20;
+const trailPersistenceTime = 20; // milliseconds
 
-let trailHistory = [];
-
-let clock = setInterval(() => {
-    if (trailHistory.length > 0) {
-        document.body.removeChild(trailHistory.shift());
-    }
-}, 20);
+let mouseTrailReaperClock = null;
 
 window.addEventListener("mousemove", (event) => {
     let trail = document.createElement("div");
     trail.className = "trail";
     trail.style.left = event.pageX - 4 + "px";
     trail.style.top = event.pageY - 4 + "px";
-    trailHistory.push(trail);
     document.body.appendChild(trail);
-    if (trailHistory.length > maxTrailLength) {
-        document.body.removeChild(trailHistory.shift());
+    if (document.getElementsByClassName("trail").length > maxTrailLength) {
+        document.body.removeChild(document.getElementsByClassName("trail")[0]);
+    }
+    if (
+        document.getElementsByClassName("trail").length > 0 &&
+        !mouseTrailReaperClock
+    ) {
+        mouseTrailReaperClock = setInterval(() => {
+            if (document.getElementsByClassName("trail").length > 0) {
+                document.body.removeChild(document.getElementsByClassName("trail")[0]);
+            }
+            if (document.getElementsByClassName("trail").length == 0) {
+                clearInterval(mouseTrailReaperClock);
+                mouseTrailReaperClock = null;
+            }
+        }, trailPersistenceTime);
     }
 });

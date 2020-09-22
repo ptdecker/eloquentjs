470ca56279b16ebc70d9b2351c064189a85a11bd
P. Todd Decker <ptdecker@mac.com>
2020-09-22 00:15:58 -0500

    Chapter 15 Solutions

--- chapter15/tabs/scripts.js
+++ chapter15/tabs/scripts.js
@@ -4,23 +4,33 @@
 
 let tabList = [];
 
+// Implemnt tabs for the passed node.
 function asTabs(node) {
     const childElements = node.children;
     let firstTab = "";
+
+    // Build a list of the names of the divs in the passed node that
+    // should be treated as tabs. Mark them as such with a proper class.
+    // And, hide all but the first one.
     for (const element of childElements) {
-        if (element.dataset.tabname) {
-            element.classList.add("tab-body");
-            tabList.push({
-                tabDiv: element,
-                name: element.dataset.tabname,
-            });
-            if (tabList.length > 1) {
-                element.style.display = "none";
-            } else {
-                firstTab = element.dataset.tabname;
-            }
+        // Skip divs that do not have a defined tag name
+        if (!element.dataset.tabname) {
+            continue;
+        }
+        element.classList.add("tab-body");
+        tabList.push({
+            tabDiv: element,
+            name: element.dataset.tabname,
+        });
+        if (tabList.length > 1) {
+            element.style.display = "none";
+        } else {
+            firstTab = element.dataset.tabname;
         }
     }
+
+    // Build the tab bar dynamically and insert it into the DOM as the first
+    // element in the passed node.
     let tabBar = document.createElement("div");
     tabBar.classList.add("tab-bar");
     for (const tabElement of tabList) {
@@ -37,15 +47,16 @@ function asTabs(node) {
     node.insertBefore(tabBar, node.firstChild);
 }
 
+// Handle opening a named tab
 function openTab(tabName) {
     for (const tabElement of tabList) {
         if (tabElement.name == tabName) {
-            tabElement.tabDiv.style.display = "initial";
+            tabElement.tabDiv.removeAttribute("style");
             tabElement.button.classList.add("tab-selected");
-        } else {
-            tabElement.tabDiv.style.display = "none";
-            tabElement.button.classList.remove("tab-selected");
+            continue;
         }
+        tabElement.tabDiv.style.display = "none";
+        tabElement.button.classList.remove("tab-selected");
     }
 }
 
